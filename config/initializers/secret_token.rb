f5b851a4dfc68a03c99423d2362a33a7fabb45c5
Sean Cuevo <seanpat09@gmail.com>
2012-07-11 16:39:24 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubersub::Application.config.secret_token = '257f4e4e65a238973fed8162a0f27b661bbc122b81dbe935e20bf21feacb2dba17477a6dfd8b7721051a3c7b80a5815622ec96e21a7283dbaab6090f264f27b3'
